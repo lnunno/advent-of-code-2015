47ec5e26fd973448593f120d0a588344232c5dae
Lucas Nunno <lucasnunno@gmail.com>
2017-01-07 13:30:03 -0700

    Attempt at parsing the Json object in Haskell.

--- day12/src/Main.hs
+++ day12/src/Main.hs
@@ -2,18 +2,39 @@
 module Main where
 
 import Data.Aeson
+import Data.Scientific
+import qualified Data.Map as M
+import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap.Lazy as H
+import qualified Data.Text as T
+
+foo :: H.HashMap String String
+foo = H.singleton "foo" "bar"
+
+sumNumbers :: Value -> Scientific
+sumNumbers (Number a) = a
+sumNumbers (Array a) = V.sum $ V.map sumNumbers a
+sumNumbers (String s) = 0
+sumNumbers (Bool b) = 0
+sumNumbers (Object o) = 0
 
 jsonFile :: FilePath
-jsonFile = "input.json"
+jsonFile = "test-input.json"
 
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
+retrieveJSON :: IO Value
+retrieveJSON = do
+    byteString <- getJSON
+    let d = decode byteString :: Maybe Value
+    case d of
+        Just json -> return json
+        Nothing -> error "Could not parse JSON file."
+
 main :: IO ()
 main = do
-    byteString <- getJSON
-    let d = decode byteString :: Maybe Object
-    case d of
-        Just obj -> print obj
-        Nothing -> error "Could not parse JSON file."
+    json <- retrieveJSON
+    print json
+    print $ sumNumbers json
