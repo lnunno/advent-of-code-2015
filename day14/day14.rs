fa93adecb32e26212f0c81bfc01f579a8e901266
Lucas Nunno <lucasnunno@gmail.com>
2017-04-01 14:44:04 -0600

    Part 2 solved.

--- day14/day14.rs
+++ day14/day14.rs
@@ -6,12 +6,16 @@ use std::fs::File;
 
 use regex::Regex;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Reindeer {
     name: String,
     speed: isize,
     speed_time: isize,
     rest_time: isize,
+    current_distance: isize,
+    points: isize,
+    rest_time_left: isize,
+    speed_time_left: isize,
 }
 
 impl Reindeer {
@@ -29,6 +33,67 @@ impl Reindeer {
             seconds -= self.rest_time;
         }
         return distance;
+    }
+
+    /// Move the race forward one second.
+    fn tick(&mut self) {
+        if self.speed_time_left > 0 {
+            self.current_distance += self.speed;
+            self.speed_time_left -= 1;
+            if self.speed_time_left == 0 {
+                self.rest_time_left = self.rest_time;
+            }
+        } else {
+            self.rest_time_left -= 1;
+            if self.rest_time_left == 0 {
+                self.speed_time_left = self.speed_time;
+            }
+        }
+    }
+}
+
+fn award_points(reindeers: &mut Vec<Reindeer>) {
+    let mut max_distance = 0;
+    for r in reindeers.iter() {
+        if r.current_distance > max_distance {
+            max_distance = r.current_distance;
+        }
+    }
+
+    for r in reindeers.iter_mut() {
+        if r.current_distance >= max_distance {
+            r.points += 1;
+        }
+    }
+}
+
+fn part1(reindeers: Vec<Reindeer>) {
+    let time_in_secs = 2503;
+    for r in reindeers.iter() {
+        println!("{:10} traveled {:4} km in {:5} seconds",
+                 r.name,
+                 r.distance_traveled(time_in_secs),
+                 time_in_secs);
+    }
+}
+
+fn part2(reindeers: Vec<Reindeer>) {
+    let mut reindeers = reindeers;
+    let time_in_secs = 2503;
+    for _ in 0..time_in_secs {
+        for r in reindeers.iter_mut() {
+            r.tick();
+        }
+
+        // Find the winning reindeer and award them a point.
+        award_points(&mut reindeers);
+    }
+    for r in reindeers.iter() {
+        println!("{:10} traveled {:4} km in {:5} seconds and received {:5} points.",
+                 r.name,
+                 r.distance_traveled(time_in_secs),
+                 time_in_secs,
+                 r.points);
     }
 }
 
@@ -54,17 +119,16 @@ fn main() {
                     speed: speed,
                     speed_time: speed_time,
                     rest_time: rest_time,
+                    current_distance: 0,
+                    points: 0,
+                    rest_time_left: 0,
+                    speed_time_left: speed_time,
                 });
 
             }
             None => panic!("No match for '{}'", line),
         }
     }
-    let time_in_secs = 2503;
-    for r in reindeers {
-        println!("{:10} traveled {:4} km in {:5} seconds",
-                 r.name,
-                 r.distance_traveled(time_in_secs),
-                 time_in_secs);
-    }
+    part1(reindeers.clone());
+    part2(reindeers);
 }
